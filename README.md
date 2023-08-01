1718441984b58d75c31cfdaa45d7d2238980aec7
konrad <konrad.kaminski@gmail.com>
2023-08-01 21:19:03 +0200

    add a draft of project to repository

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Serilog Sink for OpenObserve
 This Serilog Sink allows to log to [OpenObserve](https://openobserve.ai/).
+
+## What is this sink ?
+This project is a sink for the OpenObserver.
+
+## Quick start
+
+Register the sink in code.
+```csharp
+var logger = new LoggerConfiguration()
+    .WriteTo
+    .OpenObserve(
+        "url",
+        "organization"
+        "login",
+        "password"
+    )
+```
+
+More information about using Serilog is available in the [Serilog Documentation](https://github.com/serilog/serilog/wiki).
+
---
+++ src/Serilog.Sink.OpenObserve/OpenObserveLoggerConfigurationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Serilog.Configuration;
+using Serilog.Sinks.OpenObserve;
+using Serilog.Sinks.PeriodicBatching;
+
+namespace Serilog;
+
+public static class OpenObserveLoggerConfigurationExtensions
+{
+    public static LoggerConfiguration OpenObserve(
+        this LoggerSinkConfiguration loggerConfiguration,
+        string url,
+        string organization,
+        string login, 
+        string password,
+        string streamName = "default"
+        )
+    {
+        var sink = new OpenObserveSink(new OpenObserveHttpClient(url, organization, login, password, streamName));
+        var options = new PeriodicBatchingSinkOptions();
+        return loggerConfiguration.Sink(new PeriodicBatchingSink(sink, options));
+    }
+}
