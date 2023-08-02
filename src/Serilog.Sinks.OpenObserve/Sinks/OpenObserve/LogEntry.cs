2694d4621ac17a54bfc5f6d0caa6416d0fe9b0d7
konrad <konrad.kaminski@gmail.com>
2023-08-02 18:40:28 +0200

    missing elements

---
+++ src/Serilog.Sinks.OpenObserve/Sinks/OpenObserve/LogEntry.cs
@@ -0,0 +1,49 @@
+﻿using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Serilog.Sinks.OpenObserve;
+
+public record LogEntry(
+    DateTimeOffset TimeStamp, 
+    LogEventLevel LogLevel, 
+    string LevelName, 
+    string MessageTemplate, 
+    string Message, 
+    Dictionary<string, string> Data, 
+    string ExceptionMessage, 
+    string ExceptionStackTrace)
+{
+    public LogEntry(LogEvent @event) : this(
+        @event.Timestamp,
+        @event.Level, 
+        GetLogLevelName(@event.Level),
+        @event.MessageTemplate?.Text,
+        @event.MessageTemplate?.Render(@event.Properties),
+        GetProperties(@event.Properties),
+        @event.Exception?.Message,
+        @event.Exception?.StackTrace
+        )
+    {
+    }
+
+    private const string LOG_LEVEL_VERBOSE = "Verbose";
+    private const string LOG_LEVEL_DEBUG = "Debug";
+    private const string LOG_LEVEL_INFORMATION = "Information";
+    private const string LOG_LEVEL_WARNING = "Warning";
+    private const string LOG_LEVEL_ERROR = "Error";
+    private const string LOG_LEVEL_FATAL = "Fatal";
+
+    private static string GetLogLevelName(LogEventLevel level) => level switch
+    {
+        LogEventLevel.Verbose => LOG_LEVEL_VERBOSE,
+        LogEventLevel.Debug => LOG_LEVEL_DEBUG,
+        LogEventLevel.Information => LOG_LEVEL_INFORMATION,
+        LogEventLevel.Warning => LOG_LEVEL_WARNING,
+        LogEventLevel.Error => LOG_LEVEL_ERROR,
+        _ => LOG_LEVEL_FATAL,
+    };
+
+    private static Dictionary<string, string> GetProperties(IReadOnlyDictionary<string, LogEventPropertyValue> properties) => properties.ToDictionary(x => x.Key, x => x.Value.ToString());
+}
---
+++ src/Serilog.Sinks.OpenObserve/Sinks/OpenObserve/Sink.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Serilog.Events;
+using Serilog.Sinks.PeriodicBatching;
+
+namespace Serilog.Sinks.OpenObserve;
+
+public class Sink : IBatchedLogEventSink, IDisposable
+{
+    private readonly HttpClient _client;
+
+    public Sink(HttpClient client)
+    {
+        _client = client;
+    }
+    
+    public async Task EmitBatchAsync(IEnumerable<LogEvent> batch)
+    {
+
+        var data = batch.Select(x => new LogEntry(x));
+
+        var result = await _client.Send(data);
+        if (!result.IsSuccess)
+        {
+            throw new Exception("Incorrect response");
+        }
+    }
+
+    public Task OnEmptyBatchAsync()
+    {
+        return EmitBatchAsync(Enumerable.Empty<LogEvent>());
+    }
+
+    public void Dispose()
+    {
+    }
+}
