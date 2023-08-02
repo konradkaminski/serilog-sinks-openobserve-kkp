1de31e471a055d6f6e288735719dc4245635df8c
konrad <konrad.kaminski@gmail.com>
2023-08-02 18:40:01 +0200

    - unify log entry - response improvements

---
+++ src/Serilog.Sinks.OpenObserve/Sinks/OpenObserve/HttpClientResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Serilog.Sinks.OpenObserve;
+
+public class HttpClientResponse
+{
+    [JsonPropertyName("code")]
+    public int Code { get; set; }
+    [JsonPropertyName("error")]
+    public int Error { get; set; }
+    [JsonPropertyName("status")]
+    public HttpClientResponseStatus[] Status { get; set; }
+    public bool IsSuccess => Code == 200;
+}
