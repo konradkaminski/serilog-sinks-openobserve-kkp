bb838d83a89da5d7413058ce4f44546ae8f34fdd
konrad <konrad.kaminski@gmail.com>
2024-07-03 00:15:13 +0200

    improvements - tests

--- samples/Serilog.Sinks.OpenObserve.Sample/CustomBackgroundService.cs
+++ samples/Serilog.Sinks.OpenObserve.Sample/CustomBackgroundService.cs
@@ -18,24 +18,26 @@ public class CustomBackgroundService : IHostedService, IDisposable
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        _logger.LogInformation("{ServiceName} started.", nameof(CustomBackgroundService));
-        Console.WriteLine($"{nameof(CustomBackgroundService)} started.");
+        _logger.LogInformation("{ServiceName} started, version: {EnvVErsion}.", nameof(CustomBackgroundService),
+            Environment.Version.ToString());
         return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
         _logger.LogInformation("{ServiceName} stopped.", nameof(CustomBackgroundService));
-        Console.WriteLine($"{nameof(CustomBackgroundService)} stopped.");
         return Task.CompletedTask;
     }
 
     private void DoWork(object? state)
     {
         var count = Interlocked.Increment(ref _executionCount);
+
+        var objData = new { Counter = count, CounterMessage = $"Counter value is \"{count}\"" };
+
         _logger.LogInformation(
-            "{ServiceName} is working. Count: {Count}", nameof(CustomBackgroundService), count);
-        Console.WriteLine($"{nameof(CustomBackgroundService)} is working. Count: {count}");
+            "{ServiceName} is working. Count: {Count} with data {Data}", nameof(CustomBackgroundService), count,
+            objData);
     }
 
     public void Dispose()
