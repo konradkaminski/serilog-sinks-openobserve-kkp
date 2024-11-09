81aed975867ad4c6d903789cfb1bab74aaa0e4f2
konradkaminski <konrad.kaminski@gmail.com>
2024-11-09 11:39:09 +0100

    development/improvements (#9)
    * improvements - tests
    
    * cleanup description
    
    * update System.Text.Json, drop support of .net 6 in tests
    
    * update libraries

---
+++ Serilog.Sink.OpenObserve.Tests/Builders/LogEventBuilder.cs
@@ -0,0 +1,57 @@
+﻿using Serilog.Events;
+
+namespace Serilog.Sink.OpenObserve.Tests.Fixtures.Builders;
+
+public class LogEventBuilder
+{
+    private DateTimeOffset _dateTimeOffset = new DateTimeOffset(2024, 1, 1, 18, 20, 30, TimeSpan.Zero);
+    private LogEventLevel _logEventLevel = LogEventLevel.Debug;
+    private Exception _exception = null;
+    private MessageTemplate _messageTemplate = MessageTemplate.Empty;
+    private List<LogEventProperty> _logEventProperties = new List<LogEventProperty>();
+
+    private LogEventBuilder()
+    {
+    }
+
+    public static LogEventBuilder Init()
+    {
+        return new LogEventBuilder();
+    }
+
+    public LogEventBuilder WithDefinedDateTime(DateTimeOffset dateTimeOffset)
+    {
+        _dateTimeOffset = dateTimeOffset;
+        return this;
+    }
+
+    public LogEventBuilder WithDefinedLogLevel(LogEventLevel logEventLevel)
+    {
+        _logEventLevel = logEventLevel;
+        return this;
+    }
+
+    public LogEventBuilder WithDefinedException(Exception exception)
+    {
+        _exception = exception;
+        return this;
+    }
+
+    public LogEventBuilder WithDefinedEventProperty(LogEventProperty logEventProperty)
+    {
+        _logEventProperties.Add(logEventProperty);
+        return this;
+    }
+
+    public LogEventBuilder WithDefinedMessageTemplate(MessageTemplate messageTemplate)
+    {
+        _messageTemplate = messageTemplate;
+        return this;
+    }
+
+    public LogEvent Build()
+    {
+        return new LogEvent(
+            _dateTimeOffset, _logEventLevel, _exception, _messageTemplate, _logEventProperties);
+    }
+}
---
+++ Serilog.Sink.OpenObserve.Tests/LogEntryFormatterTests.cs
@@ -0,0 +1,119 @@
+using FluentAssertions;
+using Serilog.Events;
+using Serilog.Parsing;
+using Serilog.Sink.OpenObserve.Tests.Fixtures.Builders;
+using Serilog.Sinks.OpenObserve;
+
+namespace Serilog.Sink.OpenObserve.Tests;
+
+public class LogEntryFormatterTests
+{
+    private readonly LogEntryFormatter _logEntryFormatter = new();
+
+    [Fact]
+    public void DefaultLogEvent_ShouldBeFormatCorrectly()
+    {
+        var expected =
+            "{\"@t\":\"2024-01-01T18:20:30.0000000Z\",\"@m\":\"\",\"@mt\":\"\",\"@i\":\"00000000\",\"@l\":\"Debug\"}" +
+            Environment.NewLine;
+        var defaultEvent = LogEventBuilder.Init().Build();
+        var writer = new StringWriter();
+        _logEntryFormatter.Format(defaultEvent, writer);
+        writer.ToString().Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(LogEventLevel.Debug, "Debug")]
+    [InlineData(LogEventLevel.Error, "Error")]
+    [InlineData(LogEventLevel.Fatal, "Fatal")]
+    [InlineData(LogEventLevel.Warning, "Warning")]
+    [InlineData(LogEventLevel.Information, "Information")]
+    [InlineData(LogEventLevel.Verbose, "Verbose")]
+    public void DefaultLogEvent_WithLogLevel_ShouldBeFormatCorrectly(LogEventLevel logEventLevel,
+        string expectedLevelName)
+    {
+        var expected =
+            $"{{\"@t\":\"2024-01-01T18:20:30.0000000Z\",\"@m\":\"\",\"@mt\":\"\",\"@i\":\"00000000\",\"@l\":\"{expectedLevelName}\"}}" +
+            Environment.NewLine;
+        var defaultEvent = LogEventBuilder.Init().WithDefinedLogLevel(logEventLevel).Build();
+        var writer = new StringWriter();
+        _logEntryFormatter.Format(defaultEvent, writer);
+        writer.ToString().Should().Be(expected);
+    }
+
+    [Fact]
+    public void DefaultLogEvent_WithDefinedException_ShouldBeFormattedCorrectly()
+    {
+        var expected =
+            "{\"@t\":\"2024-01-01T18:20:30.0000000Z\",\"@m\":\"\",\"@mt\":\"\",\"@i\":\"00000000\",\"@l\":\"Error\",\"@x\":\"System.Exception: Exception message\"}" +
+            Environment.NewLine;
+        var defaultEvent = LogEventBuilder.Init().WithDefinedLogLevel(LogEventLevel.Error)
+            .WithDefinedException(new Exception("Exception message")).Build();
+        var writer = new StringWriter();
+        _logEntryFormatter.Format(defaultEvent, writer);
+        writer.ToString().Should().Be(expected);
+    }
+
+    [Fact]
+    public void DefaultLogEvent_WithDefinedValues_ShouldBeFormattedCorrectly()
+    {
+        var expected =
+            "{\"@t\":\"2024-01-01T18:20:30.0000000Z\",\"@m\":\"\",\"@mt\":\"\",\"@i\":\"00000000\",\"@l\":\"Debug\",\"name_1\":\"value_1\",\"name_2\":{\"1\":\"2\"}}" +
+            Environment.NewLine;
+        var defaultEvent = LogEventBuilder.Init()
+            .WithDefinedEventProperty(new LogEventProperty("name_1", new ScalarValue("value_1")))
+            .WithDefinedEventProperty(new LogEventProperty("name_2",
+                new DictionaryValue([
+                    new KeyValuePair<ScalarValue, LogEventPropertyValue>(new ScalarValue("1"), new ScalarValue("2"))
+                ])))
+            .Build();
+        var writer = new StringWriter();
+        _logEntryFormatter.Format(defaultEvent, writer);
+        writer.ToString().Should().Be(expected);
+    }
+
+    [Fact]
+    public void DefaultLogEvent_WithDefinedMessageTemplate_ShouldBeFormatedCorrectly()
+    {
+        var expected =
+            "{\"@t\":\"2024-01-01T18:20:30.0000000Z\",\"@m\":\"message token: \\\"First token value\\\"\",\"@mt\":\"message token: {FirstToken}\",\"@i\":\"b0bbb0c0\",\"@l\":\"Debug\",\"FirstToken\":\"First token value\"}" +
+            Environment.NewLine;
+        var defaultEvent = LogEventBuilder.Init()
+            .WithDefinedMessageTemplate(new MessageTemplate(new List<MessageTemplateToken>
+            {
+                new TextToken("message token: "),
+                new PropertyToken("FirstToken", "{FirstToken}")
+            }))
+            .WithDefinedEventProperty(
+                new LogEventProperty("FirstToken", new ScalarValue("First token value")))
+            .Build();
+        var writer = new StringWriter();
+        _logEntryFormatter.Format(defaultEvent, writer);
+        writer.ToString().Should().Be(expected);
+    }
+
+    [Fact]
+    public void DefaultLogEvent_WithDefinedMessageTemplateAndCustomTokens_ShouldBeFormatedCorrectly()
+    {
+        var expected =
+            "{\"@t\":\"2024-01-01T18:20:30.0000000Z\",\"@m\":\"Count: \\\"5\\\"with data \\\"{ Counter = 5, CounterMessage = Counter value is \\\\\\\"5\\\\\\\" }\\\"\",\"@mt\":\"Count: {Count}with data {Data}\",\"@i\":\"25613a2e\",\"@l\":\"Debug\",\"Count\":\"5\",\"Data\":\"{ Counter = 5, CounterMessage = Counter value is \\\"5\\\" }\"}" +
+            Environment.NewLine;
+        var defaultEvent = LogEventBuilder.Init()
+            .WithDefinedMessageTemplate(new MessageTemplate(new List<MessageTemplateToken>
+            {
+                new TextToken("Count: "),
+                new PropertyToken("Count", "{Count}"),
+                new TextToken("with data "),
+                new PropertyToken("Data", "{Data}")
+            }))
+            .WithDefinedEventProperty(
+                new LogEventProperty("Count", new ScalarValue("5")))
+            .WithDefinedEventProperty(
+                new LogEventProperty("Data",
+                    new ScalarValue("{ Counter = 5, CounterMessage = Counter value is \"5\" }")))
+            .Build();
+        var writer = new StringWriter();
+        _logEntryFormatter.Format(defaultEvent, writer);
+        writer.ToString().Should().Be(expected);
+    }
+}
